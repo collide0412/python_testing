1e1f3a77fdcad4a1854a2e8e0eeceea6b0d21af8
Tran Dang <dangcucvip@gmail.com>
2022-02-19 01:24:41 +0700

    working with textual data

--- python_tutor_beginners/strings.py
+++ python_tutor_beginners/strings.py
@@ -1,3 +1,18 @@
 message = 'Hello World'
 
 print(len(message))
+print(message[-1])
+print(message[0:5])
+print(message[6:])
+print(message.count('o'))
+print(message.find('Dang'))
+print(message.find('Hello'))
+message = message.replace('World', 'Dang')
+print(message)
+
+greeting = 'Hello'
+name = 'Dang'
+message = greeting + ', ' + name + '. Welcome!'
+# message = '{}, {}. Welcome!'.format(greeting, name)
+message = f'{greeting}, {name.upper()}. Welcome!'
+print(message)
