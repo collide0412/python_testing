4773939ec1f1995b3c12d20673eddfad400bae7f
Tran Dang <dangcucvip@gmail.com>
2022-02-19 10:03:04 +0700

    working with numeric data

---
+++ python_tutor_beginners/ints_and_floats.py
@@ -0,0 +1,22 @@
+num = 3.14
+print(type(num))
+
+num = 1
+num *= 10
+print(num)
+
+print(abs(-3))
+print(round(3.14, 1))
+
+num_1 = 3
+num_2 = 4
+print(num_1 == num_2)
+
+num_1 = '100'
+num_2 = '200'
+num_1 = int(num_1)
+num_2 = int(num_2)
+print(num_1 + num_2)
+# num_1 = 100
+# num_2 = 200
+# print(num_1 + num_2)
