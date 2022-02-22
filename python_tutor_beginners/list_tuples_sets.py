ba60f706fde92012f189c26cf81ff0e91d35261c
Tran Dang <dangcucvip@gmail.com>
2022-02-23 01:25:28 +0700

    working on list, tuples, sets

--- python_tutor_beginners/list_tuples_sets.py
+++ python_tutor_beginners/list_tuples_sets.py
@@ -1,9 +1,32 @@
 courses = ['History', 'Math', 'Physics', 'CompSci']
-courses = ['Art', 'Education']
-courses.append('Art')
-courses.insert(0, 'Literature')
-courses.insert(4, 'English')
-print(courses[3])
-print(courses[0:3])
-print(courses[2:])
-print(courses)
+course_str = ' - '.join(courses)
+new_list = course_str.split(' - ')
+print(course_str)
+print(new_list)
+# print(courses.index('Math')
+# for index, course in enumerate(courses, start=1):
+#     print(index, course)
+
+# print('Math' in courses)
+
+
+
+# nums = [1, 2, 5, 4, 3]
+# courses_2 = ['Art', 'Education']
+# courses.reverse()
+# courses.sort(reverse=True)
+# nums.sort(reverse=True)
+# sorted_courses = sorted(courses)
+# print(sorted_courses)
+# print(min(nums))
+# print(sum(nums))
+# print(nums)
+# popped = courses.pop()
+# print(popped)
+# courses.extend(courses_2)
+# courses.append('Art')
+# courses.insert(0, courses_2)
+# print(courses[3])
+# print(courses[0:3])
+# print(courses[2:])
+# print(courses)
---
+++ python_tutor_beginners/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(courses.index('Math'))
