1687daf164e6c862fb22bc9eb089dd19240a7ff9
Tran Dang <dangcucvip@gmail.com>
2022-02-23 20:23:25 +0700

    dictionaries - working with key-value

---
+++ python_tutor_beginners/dict.py
@@ -0,0 +1,15 @@
+student = {'name': 'John', 'age': 25, 'courses': ['Math', 'CompSci']}
+
+for key, value in student.items():
+    print(key, value)
+# age = student.pop('age')
+# del student['age']
+# student.update({'name': 'Dang', 'age': 19, 'courses': ['Math', 'CompSci', 'Art']})
+# student['phone'] = '0332192968'
+# student['name'] = 'Dang'
+# print(student['name'])
+# print(student['courses'])
+# print(student.get('phone', 'Not found'))
+# print(len(student))
+# print(student.items())
+# print(age)
